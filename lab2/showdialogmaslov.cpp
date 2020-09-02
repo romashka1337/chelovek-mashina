6d73617500e9899904bd2456469f0d11f31e8ad0
collapsing <1337romashka1337@gmail.com>
2020-09-02 15:24:49 +0700

    Revert "Update inputdialog_maslov.cpp, inputdialog_maslov.h, and 8 more files..."
    This reverts commit 9402ac99fa13c988fefb13ee682cea560ed30184.

--- lab2/showdialogmaslov.cpp
+++
@@ -1,6 +0,0 @@
-#include "showdialogmaslov.h"
-
-ShowDialogMaslov::ShowDialogMaslov()
-{
-
-}
--- lab2/startdialog_maslov.cpp
+++
@@ -1,6 +0,0 @@
-//#include "startdialog_maslov.h"
-
-//StartDialog_Maslov::StartDialog_Maslov(QWidget* pwgt)
-//{
-
-//}
